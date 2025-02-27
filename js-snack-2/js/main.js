87306cff90f5996066cb3861ad66f52a5adda1ce
MarcoVacchi <mvacchi816@gmail.com>
2025-02-27 13:19:18 +0100

    exercise complete

--- js-snack-2/js/main.js
+++ js-snack-2/js/main.js
@@ -7,8 +7,10 @@ let parola2 = prompt('inserisci una parola');
 
 if (parola1.length > parola2.length ) {
     console.log('La prima parola è la parola più lunga!' + parola1)
+    console.log('La seconda parola è la parola più corta!' + parola2)
 } else if(parola1.length < parola2.length ) {
     console.log('La seconda parola è la parola più lunga!' + parola2)
+    console.log('La prima parola è la parola più corta!' + parola1)
 } else {
         console.log('Le parole sono lunghe uguali!')
 }
--- js-snack-3/js/main.js
+++ js-snack-3/js/main.js
@@ -5,7 +5,7 @@ Il programma stampa la somma di tutti i numeri inseriti.*/
 let somma = 0;
 
 for (let i = 0; i < 10; i++) {
-  let numero = parseInt(prompt('Inserisci un numero:'));
+  let numero = parseInt(prompt('Inserisci un numero:'+(i)));
   somma += numero;
 }
 
